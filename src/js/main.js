71faed995632eacc7abf261e377453336d8860c3
Ashur Cabrera <hello@ashur.cab>
2018-10-16 17:38:50 -0700

    Remove stray line
    Missed during previous commit

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,6 @@ window.onload = () =>
 	let app = new App( document.getElementById( ( 'app' ) ) );
 
 
-	app.appendChild( scene );
 	app.scene = new Scene( 'https://images.unsplash.com/photo-1498637841888-108c6b723fcb?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=54d1fa94e704450bcdd1aa84dae243cf&auto=format&fit=crop&w=2691&q=80' );
 
 	window.app = app;
