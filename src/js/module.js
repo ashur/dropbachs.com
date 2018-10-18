e37fc72406f81bdbc82681c1f6b8562526ac1f47
Ashur Cabrera <hello@ashur.cab>
2018-10-18 15:53:10 -0700

    Add Module.transition
    Improves coordination when defining multiple transition properties on a single element

--- src/js/module.js
+++ src/js/module.js
@@ -27,6 +27,45 @@ class Module
 
 		return element;
 	}
+
+	transition( property, duration, timingFunction )
+	{
+		let newTransition = `${property} ${duration} ${timingFunction}`;
+		let newTransitions = [];
+
+		let originalTransitions = this.element.style.transition.split( ',' );
+
+		let didAdd = false;
+		originalTransitions.forEach( originalTransition =>
+		{
+			if( originalTransition == "" )
+			{
+				// Skip blanks
+				return;
+			}
+
+			let definition = originalTransition.split( ' ' );
+			if( definition[0] == property )
+			{
+				// Property was already defined, let's replace it
+				newTransitions.push( newTransition );
+				didAdd = true;
+			}
+			else
+			{
+				// Unrelated definition, make sure we keep it
+				newTransitions.push( originalTransition );
+			}
+		});
+
+		if( !didAdd )
+		{
+			// First time this property has been added
+			newTransitions.push( newTransition );
+		}
+
+		this.element.style.transition = newTransitions.join( ', ' );
+	}
 }
 
 module.exports = Module;
