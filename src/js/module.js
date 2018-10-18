239deedf4297257dea789c3ed89ecacc9bf992e1
Ashur Cabrera <hello@ashur.cab>
2018-10-18 15:52:19 -0700

    Remove old Module cruft

--- src/js/module.js
+++ src/js/module.js
@@ -3,12 +3,6 @@ class Module
 	constructor( element )
 	{
 		this.element = element;
-		this.playbackRate = 1;
-
-		this.units = {
-			x: 'vw',
-			y: 'vh',
-		};
 	}
 
 	appendChild( childModule )
@@ -32,113 +26,6 @@ class Module
 		} );
 
 		return element;
-	}
-
-	set left( left )
-	{
-		this.element.style.left = `${left}${this.units.x}`;
-	}
-
-	get left()
-	{
-		return this.boundingClientRect.left;
-	}
-
-	/**
-	 * @param {number} x		X position (in viewport units)
-	 *
-	 * @param {number} y		Y position (in viewport units)
-	 *
-	 * @param {number} duration	Animation duration (ms)
-	 *
-	 * @param {string} easing	Easing function (ex., 'linear')
-	 *
-	 * @return	{Promise}
-	 */
-	moveBy( xOffset, yOffset, duration, easing )
-	{
-		// Convert from pixels to vw/vh
-		let boundingRect = this.element.getBoundingClientRect();
-
-		let originalX = boundingRect.left / window.innerWidth * 100;
-		let originalY = boundingRect.top / window.innerHeight * 100;
-
-		let x = xOffset == 0 ? null : this.left + xOffset;
-		let y = yOffset == 0 ? null : this.top + yOffset;
-
-		return this.moveTo( x, y, duration, easing );
-	}
-
-	/**
-	 * @param {number} x		X position (in viewport units)
-	 *
-	 * @param {number} y		Y position (in viewport units)
-	 *
-	 * @param {number} duration	Animation duration (ms)
-	 *
-	 * @param {string} easing	Easing function (ex., 'linear')
-	 *
-	 * @return	{Promise}
-	 */
-	moveTo( x, y, duration, easing )
-	{
-		this.element.classList.add( easing );
-
-		let adjustedDuration = duration / this.playbackRate;
-		this.element.style.transitionDuration = `${adjustedDuration}ms`;
-
-		return new Promise( ( resolve, reject ) =>
-		{
-			if( x != undefined )
-			{
-				this.left = x;
-			}
-			if( y != undefined )
-			{
-				this.top = y;
-			}
-
-			setTimeout( () =>
-			{
-				resolve();
-
-			}, adjustedDuration );
-		} );
-	}
-
-	/**
-	 * Convert bounding rect properties to viewport units
-	 */
-	get boundingClientRect()
-	{
-		let boundingRect = this.element.getBoundingClientRect();
-		let viewportRect = {};
-
-		let viewportWidth = document.documentElement.clientWidth;
-		let viewportHeight = document.documentElement.clientHeight;
-
-		viewportRect.top = boundingRect.top / viewportHeight * 100;
-		viewportRect.right = boundingRect.right / viewportWidth * 100;
-		viewportRect.bottom = boundingRect.bottom / viewportHeight * 100;
-		viewportRect.left = boundingRect.left / viewportWidth * 100;
-
-		viewportRect.height = boundingRect.height / viewportHeight * 100;
-		viewportRect.width = boundingRect.width / viewportWidth * 100;
-
-		viewportRect.x = viewportRect.left;
-		viewportRect.y = viewportRect.top;
-
-		return viewportRect;
-	}
-
-	set top( top )
-	{
-		this.element.style.top = `${top}${this.units.y}`;
-	}
-
-	get top()
-	{
-		return this.boundingClientRect.top;
 	}
 }
 
